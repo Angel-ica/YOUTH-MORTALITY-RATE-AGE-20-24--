4e4351b868d4751cd515ef329b66aecc88b4be4b
Angelica Aluo <angelicaaluo@Angelicas-MacBook-Air.local>
2022-11-06 18:26:29 +0000

    updates file

--- data_collection.py
+++ data_collection.py
@@ -2,14 +2,10 @@ from selenium import webdriver
 import time 
 import pandas as pd 
 import urllib.request
-import numpy as np
 
 data=pd.read_csv('API_SH.DYN.2024_DS2_en_csv_v2_4543808.csv',sep=',')
 new_data=data.dropna(axis=1,how='all')
 data_csv=new_data.to_csv('hiv.csv')
-df=pd.DataFrame(new_data)
-smaller_data=(df.loc[:,['Country Name','2015','2016','2017','2018','2019','2020']])
-smaller_data.to_csv('hiv_2.csv')
 
 #try:
 # rising_cases=[]
